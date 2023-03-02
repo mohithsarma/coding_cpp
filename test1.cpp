516ac76c2b0e4c7a5d56ca967bb402c14dc3701a
mohithsarma <mohith.sarma@iiitb.org>
2023-03-02 21:19:33 +0530

    added .gitignore to test

--- test1.cpp
+++ test1.cpp
@@ -1,8 +1,8 @@
 // this is a test file for uploading 
 // sample code for cpp
 #include <iostream> 
-
+using namespace std;
 int main(){
-    cout<<"welcome to C++ world"
+    cout<<"welcome to C++ world";
     return 0;
 }
